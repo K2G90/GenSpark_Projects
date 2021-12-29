437f0c08d8a5147cf861822d50761b280758c6b1
Cedric L. Williams Jr <clw.jr90@gmail.com>
2021-12-29 17:14:42 -0500

    added a while loop & do while to compensate for number of tries and game plays

--- guess_the_number/src/com/company/Main.java
+++ guess_the_number/src/com/company/Main.java
@@ -5,35 +5,36 @@ public class Main {
 
     public static void main(String[] args) {
         int tries = 6;
-
+        boolean gamePlay;
 //
         Scanner inputTwo = new Scanner(System.in);
         String numberGuess = inputTwo.nextLine();
         int guess = Integer.parseInt(numberGuess);
-
         int ranNum = (int)(Math.random() * 20 + 1); //calculates random number between 1 and 20
-    do {
-        System.out.println("Yo what's up? What's your name?");
-        Scanner inputOne = new Scanner(System.in);
-        String userName = inputOne.nextLine();
 
-        System.out.println("Well alright " + userName + ", I am thinking of a number between 1 and 20.");
-        System.out.print("Do you think you can guess it?");
+        while(gamePlay = true;){
+            do {
+                System.out.println("Yo what's up? What's your name?");
+                Scanner inputOne = new Scanner(System.in);
+                String userName = inputOne.nextLine();
 
-        if (guess == ranNum) {
-            System.out.println("Good Job " + userName + "! You guessed correctly!");
-            System.out.println("Care to have another go?");
+                System.out.println("Well alright " + userName + ", I am thinking of a number between 1 and 20.");
+                System.out.print("Do you think you can guess it?");
 
-        } else if (guess < ranNum) {
-            System.out.println("Dang it!, you guessed too low. Try again");
-            tries--;
-        } else if (guess > ranNum) {
-            System.out.println("Whoa! You guessed a tad bit too high. Try again.");
-            tries--;
+                if (guess == ranNum) {
+                    System.out.println("Good Job " + userName + "! You guessed correctly!");
+                    System.out.println("Care to have another go?");
+
+                } else if (guess < ranNum) {
+                    System.out.println("Dang it!, you guessed too low. Try again");
+                    tries--;
+                } else if (guess > ranNum) {
+                    System.out.println("Whoa! You guessed a tad bit too high. Try again.");
+                    tries--;
+
+                }
+            } while ((guess != ranNum) && (tries > 0));
 
         }
-    } while((guess!=ranNum) && (tries > 0));
-
-
     }
 }
