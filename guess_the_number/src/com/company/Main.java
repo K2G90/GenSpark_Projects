9db79faab7b97b5b661d82a6dc3c85be8a6e4b34
Cedric L. Williams Jr <clw.jr90@gmail.com>
2021-12-29 13:23:00 -0500

    created second user input(number guess) still has bugs

--- guess_the_number/src/com/company/Main.java
+++ guess_the_number/src/com/company/Main.java
@@ -7,5 +7,14 @@ public class Main {
         System.out.println("Yo what's up? What's your name?");
         Scanner inputOne = new Scanner(System.in);
         String userName = inputOne.nextLine();
+
+        System.out.println("Well alright " + userName + ", I am thinking of a number between 1 and 20.");
+        System.out.print("Do you think you can guess it?");
+
+        Scanner inputTwo = new Scanner(System.in);
+        String numberGuess = inputTwo.nextLine();
+
+
+        if(Integer.parseInt(numberGuess) == )
     }
 }
