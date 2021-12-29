e5e9e666d0878eb2060a9032b7ae6a16fc138f70
Cedric L. Williams Jr <clw.jr90@gmail.com>
2021-12-28 19:59:03 -0500

    updated beginning text

--- dragon_cave/src/com/company/Main.java
+++ dragon_cave/src/com/company/Main.java
@@ -5,7 +5,14 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner enterCave = new Scanner(System.in); //Creates a Scanner object
+
+        System.out.println("You are in a land full of Dragons.");
+        System.out.println("In Front of you, you see two caves. In one cave, ");
+        System.out.println("the dragon is friendly and will share his treasure with ");
+        System.out.println("you. The other dragon is greedy and hungry and will eat you");
+        System.out.println("on sight. Which will you choose? (1 or 2)");
+
         String inPut = enterCave.nextLine();
-        System.out
+        System.out.println("You have chosen path: " + inPut);
     }
 }
