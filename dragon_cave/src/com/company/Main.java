3b219dc69e24529f540a48349f6fd5cb45d62e97
Cedric L. Williams Jr <clw.jr90@gmail.com>
2021-12-28 19:54:20 -0500

    created user input

--- dragon_cave/src/com/company/Main.java
+++ dragon_cave/src/com/company/Main.java
@@ -1,8 +1,11 @@
 package com.company;
-
+import java.util.Scanner; //Brings in the Scanner Class
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Scanner enterCave = new Scanner(System.in); //Creates a Scanner object
+        String inPut = enterCave.nextLine();
+        System.out
     }
 }
