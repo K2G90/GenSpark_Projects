ca3b75a96dbdc60dd10a651dfde2f6f2b05aa8a4
Cedric L. Williams Jr <clw.jr90@gmail.com>
2021-12-29 11:13:56 -0500

    upload guess game

---
+++ guess_the_number/src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+import java.util.Scanner;  //brings in Scanner class to be used as an input method
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
