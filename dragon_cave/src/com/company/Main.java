daf0cdcf1c33403000c2eb2a87c42b99cdc22a06
Cedric L. Williams Jr <clw.jr90@gmail.com>
2021-12-28 20:18:06 -0500

    Project3 complete

--- dragon_cave/src/com/company/Main.java
+++ dragon_cave/src/com/company/Main.java
@@ -14,16 +14,21 @@ public class Main {
 
         String inPut = enterCave.nextLine();
         System.out.println("You have chosen path: " + inPut); //Displays the option user chose.
+        try {
+            switch (inPut) {
 
-        switch (inPut) {
-
-            case "1" -> System.out.println("Hey there!, you want to be my friend and share my treasures?!");
-            case "2" -> {
-                System.out.println("You approach the case...");
-                System.out.println("It is dark and spooky...");
-                System.out.println("A large dragon jumps out in front of you! He open his jaws and...");
-                System.out.println("Gobbles you down in one bite!");
+                case "1" -> System.out.println("Hey there!, you want to be my friend and share my treasures?!");
+                case "2" -> {
+                    System.out.println("You approach the case...");
+                    System.out.println("It is dark and spooky...");
+                    System.out.println("A large dragon jumps out in front of you! He open his jaws and...");
+                    System.out.println("Gobbles you down in one bite!");
+                }
+                default -> throw new Exception();
+            }
+        } catch(Exception e) {
+                if(inPut.equals("")) System.out.println("Please choose an option!");
+                else System.out.println("Please choose Path 1 or 2.");
             }
         }
     }
-}
