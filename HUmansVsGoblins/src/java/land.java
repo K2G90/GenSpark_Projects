8b4f028fad5ffb43fd1af7bef284cbbe942f5580
Cedric L. Williams Jr <clw.jr90@gmail.com>
2022-01-13 17:39:16 -0500

    Updated land(still figuring out methods)

--- HUmansVsGoblins/src/java/land.java
+++ HUmansVsGoblins/src/java/land.java
@@ -1,7 +1,40 @@
 package java;
 
+import java.util.HashMap;
+import java.util.Map;
+
+
 public class land {
-    public String main(String[] args){
+    private String level;
+    private boolean hasTreasure;
+    private int north, south, east, west;
+    private HashMap<String, Integer> navigation;
+
+
+
+
+    public land (String level, boolean hasTreasure, int n, int s, int e, int w) {
+            this.level = level;
+            this.hasTreasure = hasTreasure;
+            this.north= n;
+            this.south = s;
+            this.east= e;
+            this.west = w;
+
+            navigation = new HashMap<>();
+    }
+
+    public land(){
+        this.level = "";
+        this.hasTreasure = false;
+    }
+
+
+
+
+    public String toString(){
+        return "name: " + name
+                + "\n treasure" + ;
 
     }
 }
