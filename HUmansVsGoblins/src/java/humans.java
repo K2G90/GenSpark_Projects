08a091c68852dc8d639feb254d818cfec7d964b6
Cedric L. Williams Jr <clw.jr90@gmail.com>
2022-01-16 20:14:05 -0500

    Updated to package17 for switch clase

--- HUmansVsGoblins/src/java/humans.java
+++ HUmansVsGoblins/src/java/humans.java
@@ -2,31 +2,44 @@ package java;
 
 public class humans {
     private int health = 100, attack = 15, defense= 10;
-    
-    public humans (){}
+    public String playLevel;
 
+    //Default constructor
+    public humans () {}
     public humans (int h, int a, int d) {
         health = h;
         attack = a;
         defense = d;
     }
+
     //Getters and Setters for Humans Attributes/Fields
     public int getHealth(){return health;}
-    public void setHealth(int health) {
-        this.health = health;
-    }
+    public void setHealth(int health) {this.health = health;}
 
     public int getAttack() {return attack;}
-    public void setAttack(int attack) {
-        this.attack = attack;
-    }
+    public void setAttack(int attack) {this.attack = attack;}
 
     public int getDefense() {return defense;}
-    public void setDefense(int defense) {
-        this.defense = defense;
+    public void setDefense(int defense) {this.defense = defense;}
+
+    public static humans level(String level) {
+        new humans();
+
+        return switch (level) {
+            case "easy" -> new humans(100, 15, 10);
+            case "medium" -> new humans(80, 10, 5);
+            case "hard" -> new humans(60, 5, 0);
+            default -> new humans();
+        };
     }
 
-    public String main(String[] args){
-
+    //Override toString Method to display instance of Object
+    @Override
+    public String toString() {
+        return "Humans {" +
+                "\n health = " + health +
+                "\n attack = " + attack +
+                "\n defense = " + defense +
+                "\n }";
     }
 }
