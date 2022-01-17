0ca9bd963468a64ed1eef8d80d337e89fdab397f
Cedric L. Williams Jr <clw.jr90@gmail.com>
2022-01-17 08:26:21 -0500

    Updated window class

--- HUmansVsGoblins/src/java/Window.java
+++ HUmansVsGoblins/src/java/Window.java
@@ -1,2 +1,12 @@
-package java;public class Window {
+package java;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Window {
+    public Window(int width, int height, String title, Main main){
+        JFrame frame = new JFrame(title);
+
+        frame.setPreferredSize(new Dimension(width, height));
+    }
 }
