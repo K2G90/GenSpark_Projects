f788091a83e9af56b8ff359e96aa2d218356db67
Cedric L. Williams Jr <clw.jr90@gmail.com>
2022-02-07 23:29:26 -0500

    Updated

--- HUmansVsGoblins/src/java/Window.java
+++ HUmansVsGoblins/src/java/Window.java
@@ -8,5 +8,9 @@ public class Window {
         JFrame frame = new JFrame(title);
 
         frame.setPreferredSize(new Dimension(width, height));
+        frame.setMaximumSize(new Dimension(width, height));
+        frame.setMinimumSize(new Dimension(width, height));
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
